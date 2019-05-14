52bada73f85953c7072c429a645f3bb85d64cab7
Lan Niu <ln46@students.waikato.ac.nz>
2019-05-14 02:27:29 +0000

    test12

--- tests/server/apiv1.spec.js
+++ tests/server/apiv1.spec.js
@@ -41,7 +41,7 @@
       assert(resMock.status.lastCall.calledWith(400), 'Unexpected status code:' + resMock.status.lastCall.args);
     });
 
-    it('with valid city name and error from request call', function() {
+    it('with valid zip code and error from request call', function() {
       reqMock = {
         query: {
           zip: "Hamilton"
