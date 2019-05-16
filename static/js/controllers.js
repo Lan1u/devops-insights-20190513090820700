63a8ab443d2b06305cdd6ad8c848306d41060919
Lan Niu <ln46@students.waikato.ac.nz>
2019-05-16 03:02:54 +0000

    test2-18

--- static/js/controllers.js
+++ static/js/controllers.js
@@ -1,5 +1,5 @@
-
 var ConsoleModule = angular.module('ConsoleModule', ['ngRoute']);
+   
 
 ConsoleModule.config(['$routeProvider', '$locationProvider','$sceDelegateProvider', '$httpProvider',
     function ($routeProvider, $locationProvider, $sceDelegateProvider, $httpProvider) {
@@ -16,16 +16,79 @@ ConsoleModule.controller('wcontroller', ['$scope', '$http', '$routeParams', '$ti
     $scope.somemessage = "Some weather";
     $scope.zip1City = "";
     $scope.zip1Weather = "";
-	
-	var a,b,c,d;
-	
-	var map=new google.maps.Map(document.getElementById('googleMap'),{
-		zoom:4,
-		center:{lat:-36.8486,lng:174.7633}});
-	
-		
-    $scope.zip = function(which) {
+    var a,b,c,d;
+    var marker;
+    var txt=null;
+  
 
+                var map = new google.maps.Map(document.getElementById('googleMap'), {
+                          zoom: 4,
+                        center: {lat: -36.8485, lng: 174.7633}
+                    });
+    google.maps.event.addListener(map, 'click', function(event) {
+        var myLatLng = event.latLng;
+        var lat = myLatLng.lat();
+        var lon = myLatLng.lng();
+        //alert(lat);
+        //alert(typeof lon);
+           if(lat<-35.22676 && lat>-46.56069 && lon>166.55973 && lon<178.00417){
+        placeMarker(event.latLng);
+        }
+
+  
+    });
+function addInfoWindow(marker, message) {
+
+            var infoWindow = new google.maps.InfoWindow({
+                content: message
+            });
+
+           
+                infoWindow.open(map, marker);
+           
+        }
+
+function placeMarker(location) {
+        marker = new google.maps.Marker({
+        position: location,  
+        map: map,
+      
+    });  
+     var lat= marker.getPosition().lat();
+     var lon= marker.getPosition().lng();
+   
+   
+//    if(lat<-35.22676 && lat>-46.56069 && lon>-176.55973 && lon<178.00417){
+   
+    var st=lat+'&lon='+lon+'&appid=b6907d289e10d714a6e88b30761fae22';
+    $http({
+                method: "GET",
+                url: 'https://openweathermap.org/data/2.5/weather?lat='+st
+               
+            })
+            .then( function(response) {
+                var y=JSON.stringify(response); var z=y.split('"'); var i=0;
+                while(z[i]!=="main"){
+                i++;
+                }
+                var k=0;
+                while(z[k]!=="temp"){
+                k++;
+                }
+               
+                    txt="Weather is:  "+z[i+2]+"  temperature is:  "+z[k+1];
+                    //alert(txt);
+                    addInfoWindow(marker, txt);
+            });
+    // }  
+
+             
+}
+
+
+
+    $scope.zip = function(which) {
+      
         var data = "";
         if(which === 1) {
             data = $scope.zip1m;
@@ -35,70 +98,58 @@ ConsoleModule.controller('wcontroller', ['$scope', '$http', '$routeParams', '$ti
             data = $scope.zip3m;
         } else if(which === 4) {
             data = $scope.zip4m;
-        } 
+        }
 
-        if(data.length >=4) {
+        if(data.length >= 4) {
+          
             $http({
                 method: "GET",
                 url: '/api/v1/getWeather?zip=' + data
             }).then( function(response) {
-            	var myLatLng=null;
-            	
+                var myLatLng=null;
+              
                 if(which === 1) {
                     $scope.zip1City = response.data.city;
                     $scope.zip1Weather = response.data.weather;
-                    myLatLng={lat:response.data.la,lng:response.data.lo};
-                    
-                    a=new google.maps.Marker({position:myLatLng,map:map});
-                    
-                    
-                    /*var conn = new ActiveXObject("ADODB.Connection");
-				var rs = new ActiveXObject("ADODB.Recordset"); 
-                var connectionstring = "Driver={IBM DB2 ODBC DRIVER};Database=BLUDB;Hostname=dashdb-txn-sbox-yp-dal09-03.services.dal.bluemix.net;Port=50000;Protocol=TCPIP;Uid=scq49262;Pwd=Woaizhuying1995.;";  
-				conn.open(connectionstring);  
-                    var sql = " insert into Req values('Auckland','100000');";
-                rs=conn.execute(sql);
-                rs.close(); 
-                conn.close();
-                    */
-                   var ibmdb = require('ibm_db');
-                   ibmdb.open("DATABASE=BLUDB;HOSTNAME=	dashdb-txn-sbox-yp-dal09-03.services.dal.bluemix.net;UID=scq49262;PWD=Woaizhuying.;PORT=	dashdb-txn-sbox-yp-dal09-03.services.dal.bluemix.net;PROTOCOL=TCPIP", function (err,conn) {
-  if (err) return console.log(err);
-  
-  conn.query("insert into Req values('Auckland','100000');", function (err, data) {
-    if (err) console.log(err);
-    else console.log(data);
- 
-    conn.close(function () {
-      console.log('done');
-    });
-  });
-});
-                   
+                    myLatLng = {lat: response.data.la, lng: response.data.lo};          
+
+                    a= new google.maps.Marker({
+                         position: myLatLng,
+                        map: map,
+                        
+                    });
+                 
                 } else if(which === 2) {
                     $scope.zip2City = response.data.city;
                     $scope.zip2Weather = response.data.weather;
-                     myLatLng={lat:response.data.la,lng:response.data.lo};
-                    
-                    b=new google.maps.Marker({position:myLatLng,map:map});
+                    myLatLng = {lat: response.data.la, lng: response.data.lo};          
+
+                    b = new google.maps.Marker({
+                         position: myLatLng,
+                        map: map,
+                        
+                    });
                 } else if(which === 3) {
                     $scope.zip3City = response.data.city;
                     $scope.zip3Weather = response.data.weather;
-                     myLatLng={lat:response.data.la,lng:response.data.lo};
-                    
-                    c=new google.maps.Marker({position:myLatLng,map:map});
+                    myLatLng = {lat: response.data.la, lng: response.data.lo};          
+
+                    c = new google.maps.Marker({
+                         position: myLatLng,
+                        map: map,
+                        
+                    });
                 } else if(which === 4) {
                     $scope.zip4City = response.data.city;
                     $scope.zip4Weather = response.data.weather;
-                     myLatLng={lat:response.data.la,lng:response.data.lo};
-                    
-                    d=new google.maps.Marker({position:myLatLng,map:map});
-                } 
-                
-                //sql part
-				
-            	
-              
+                    myLatLng = {lat: response.data.la, lng: response.data.lo};          
+
+                    d = new google.maps.Marker({
+                         position: myLatLng,
+                        map: map,
+                        
+                    });
+                }
             });
         } else {
             if(which === 1) {
@@ -113,8 +164,9 @@ ConsoleModule.controller('wcontroller', ['$scope', '$http', '$routeParams', '$ti
                 } else if(which === 4) {
                     $scope.zip4City = "";
                     $scope.zip4Weather = "";
-                } 
+                }
         }
     };
-    
+  
 }]);
+
