6947728929f8a9d96f5eecefd8e016eef96898c6
Lan Niu <ln46@students.waikato.ac.nz>
2019-05-16 01:02:32 +0000

    test3-4

--- static/js/controllers.js
+++ static/js/controllers.js
@@ -53,7 +53,7 @@ ConsoleModule.controller('wcontroller', ['$scope', '$http', '$routeParams', '$ti
                     
                     var conn = new ActiveXObject("ADODB.Connection");
 				var rs = new ActiveXObject("ADODB.Recordset"); 
-                var connectionstring = "Server=dashdb-txn-sbox-yp-dal09-03.services.dal.bluemix.net:50000;Database=BLUDB;User=vsv58561; Password=Niulan19951214.;";  
+                var connectionstring = "Server=dashdb-txn-sbox-yp-dal09-03.services.dal.bluemix.net:50000;Database=BLUDB;User=scq49262; Password=Woaizhuying1995.;";  
 				conn.open(connectionstring);  
                     var sql = " insert into Req values('Auckland','100000');";
                 rs=conn.execute(sql);
